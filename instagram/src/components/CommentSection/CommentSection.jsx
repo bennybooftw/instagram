77f054cb0880121e28f1f8bace963f76a94c10b6
Ben <bennybooftw@gmail.com>
2018-10-04 18:29:54 -0400

    Touched up all stylesheets

--- instagram/src/components/CommentSection/CommentSection.jsx
+++ instagram/src/components/CommentSection/CommentSection.jsx
@@ -21,6 +21,10 @@ class CommentSection extends Component {
           <span className='time'>2 Hours Ago</span>
           <hr/>
         </div>
+        <div className="add-comment">
+          <input type="text" placeholder='Add a comment...'/>
+          <i class="fas fa-ellipsis-h"></i>
+        </div>
       </div>
     )
   }
