117d5f80717e0e39023a81ea7603310ced9d5317
Ben <bennybooftw@gmail.com>
2018-10-09 23:11:41 -0400

    Added Post component and Post css

---
+++ instagram/src/components/PostContainer/Post.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Post.css';
+
+const Post = props => {
+  return(
+    <div className="post">
+      <div className="profile">
+        <img src={props.thumbnail} alt="" />
+        <p>{props.username}</p>
+      </div>
+      <div className="picture">
+        <img src={props.imageUrl} alt="" />
+      </div>
+      <div className="icons">
+        <i className="far fa-heart" onClick={props.likeClickHandler} />
+        <i className="far fa-comment" onClick={props.commentClickHandler} />
+      </div>
+      <p className="likes">{props.likes} likes</p>
+    </div>
+  )
+}
+
+export default Post;
