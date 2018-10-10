1b9cd7e3d3af5c09a9d2f93179ec9dd6dec185f5
Ben <bennybooftw@gmail.com>
2018-10-09 23:05:58 -0400

    Setup handler functions and componentDidMount for App Component

--- instagram/src/App.jsx
+++ instagram/src/App.jsx
@@ -7,21 +7,56 @@ import dummyData from './dummy-data.js'
 class App extends Component {
   constructor() {
     super();
-    this.state = { dummyData }
+    this.state = {
+      data: [],
+      input: ''
+    }
   }
 
+  searchInputHandler = event => {
+    const value = event.target.value;
+    this.setState({
+      input: value
+    })
+  }
+
+  componentDidMount() {
+    this.setState({
+      data: dummyData
+    })
+  }
+
+  searchHandler = event => {
+    if (event.key === "Enter") {
+      this.setState({
+        data: this.state.data.filter(post => {
+          return post.username === this.state.input;
+        })
+      });
+    }
+  }
 
   render() {
     return (
-      <div className='App'>
-        <SearchBar />
-        <>
-          {this.state.dummyData.map((post) => {
-            return(
-              <PostContainer key={post.timestamp} data={post} />
-            )
-          })}
-        </>
+      <div>
+        <SearchBar 
+          posts={this.state.data}
+          searchHandler={this.searchHandler}
+          searchInputHandler={this.searchInputHandler}
+        />
+        {this.state.data.map(post => {
+          return(
+            <PostContainer 
+              key={post.timestamp} 
+              data={this.state.data}
+              thumbnail={post.thumbnailUrl}
+              username={post.username}
+              imageUrl={post.imageUrl}
+              likes={post.likes}
+              comments={post.comments}
+            />
+          )
+        })}
       </div>
     );
   }
