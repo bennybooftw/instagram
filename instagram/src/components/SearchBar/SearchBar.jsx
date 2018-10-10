f2e4fbde427f8579ec44753e17288230f8478015
Ben <bennybooftw@gmail.com>
2018-10-09 23:10:11 -0400

    Restructured SearchBar Component to a Class Component

--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchBar.css';
 
-	
-const SearchBar = () => {
-  return (
-    <div className='search-container'>
-      <div className="search-logo">
-        <i className="fab fa-instagram"></i>  
-        <p className='instagram-text'>Instagram</p>
+class SearchBar extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+  
+   onFocus = event => {
+    event.target.value = "";
+    event.target.classList.add("no-background");
+  };
+
+  onBlur = event => {
+    event.target.value = "Search";
+    event.target.classList.remove("no-background");
+  };
+
+   render() {
+    return (
+      <div className="search-bar">
+        <div className="logo">
+          <div className="logo-img">
+            <i className="fab fa-instagram" />
+          </div>
+          <div className="logo-name">Instagram</div>
+        </div>
+        <input
+          type="search"
+          className="search"
+          defaultValue="Search"
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          onChange={this.props.searchInputHandler}
+          onKeyDown={this.props.searchHandler}
+        />
+        <div className="icons">
+          <i className="far fa-compass" />
+          <i className="far fa-heart" />
+          <i className="far fa-user" />
+        </div>
       </div>
-      <input className='input-search' placeholder="🔍 Search" />
-      <div className="nav-icons">
-      <i className="far fa-compass fa-lg" />
-      <i className="far fa-heart fa-lg" />
-      <i className="far fa-user fa-lg" />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
  
 export default SearchBar;
