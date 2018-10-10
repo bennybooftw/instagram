af11bbfd95ee1abdd3e402ec46551bba41ffba86
Ben <bennybooftw@gmail.com>
2018-10-09 23:13:19 -0400

    Added Comment Component

---
+++ instagram/src/components/CommentSection/Comment.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CommentSection.css';
+
+const Comment = props => {
+  return(
+    <div className="comment">
+      <p className='user'>{props.commentUser}</p>
+      <p className="comment-text">{props.comment}</p>
+    </div>
+  )
+}
+
+export default Comment;
