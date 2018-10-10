2ec2dc9e5786e41071574226ad438e6e3fe8ae1b
Ben <bennybooftw@gmail.com>
2018-10-09 23:26:11 -0400

    Restructured PostContainer Component

--- instagram/src/components/PostContainer/PostContainer.jsx
+++ instagram/src/components/PostContainer/PostContainer.jsx
@@ -1,21 +1,44 @@
 import React, { Component } from "react";
-import CommentSection from '../CommentSection/CommentSection';
 import "./PostContainer.css";
+import CommentSection from "../CommentSection/CommentSection";
+import Post from "./Post";
 
-class PostContainer extends Component {
+ class PostContainer extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      thumbnailUrl: props.thumbnail,
+      username: props.username,
+      imageUrl: props.imageUrl,
+      likes: props.likes
+    };
+  }
+  
+   likeClickHandler = event => {
+    event.preventDefault();
+    this.setState({ likes: this.state.likes + 1 });
+  };
+
+  commentClickHandler = event => {
+    event.preventDefault();
+    document.querySelector(".add-comment").focus();
+  };
 
   render() {
-    return(
-      <div className='post-container'>
-        <div className="img-logo-wrapper">
-          <img src={this.props.data.thumbnailUrl} />
-          <p>{this.props.data.username}</p>
-        </div>
-        <img src={this.props.data.imageUrl} />
-        <CommentSection data={this.props.data.comments} likes={this.props.data.likes}/>
+    return (
+      <div className="card">
+        <Post
+          thumbnail={this.state.thumbnailUrl}
+          username={this.state.username}
+          imageUrl={this.state.imageUrl}
+          likes={this.state.likes}
+          likeClickHandler={this.likeClickHandler}
+          commentClickHandler={this.commentClickHandler}
+        />
+        <CommentSection comments={this.props.comments} time={this.props.time} />
       </div>
-    )
+    );
   }
 }
 
-export default PostContainer;
+ export default PostContainer;
